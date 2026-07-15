b33868f00f520e08dd499ea6cad4927ce3794d9b
geniruphin-junior <geniruphin@gmail.com>
2026-07-15 18:14:04 +0200

    feat:pallette colors

--- utils/visualiseur/color.py
+++ utils/visualiseur/color.py
@@ -1,8 +1,11 @@
-cyberpunk_colors = [
-    "#FF007F",
-    "#00F0FF",
-    "#9D00FF",
-    "#39FF14",
-    "#FF00F0",
-    "#FF0033",
-]
+cyberpunk_color = {
+    "pinkred": "#FF007F",
+    "cyan": "#00F0FF",
+    "violet": "#9D00FF",
+    "green": "#39FF14",
+    "pink": "#FF00F0",
+    "red": "#FF0033",
+}
+lés = cyberpunk_colors.keys
+val = cyberpunk_colors.values
+print(lés)
