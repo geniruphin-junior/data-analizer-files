1dcbb1df0bae94b1f334e85e22f8c244872701e4
geniruphin-junior <geniruphin@gmail.com>
2026-07-16 08:41:24 +0200

    feat(main.py):creation dde l'appli coté terminal

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from pathlib import Path
+import os
+from utils.load_file import load_file
+
+file = input("Voulez vous analyser quel fichier à partir du dossier data ? : ")
+folder = "data"
+root = os.path.join(folder, file)
+
+try:
+    if os.path.exists(root):
+        df = load_file(root)
+    else:
+        raise FileNotFoundError
+except FileNotFoundError:
+    print("Fichier non existant")
