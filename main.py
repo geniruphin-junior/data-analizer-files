7ce64c62ef447ff2cac3fb3c89de485a1b18e5b0
geniruphin-junior <geniruphin@gmail.com>
2026-07-22 12:27:29 +0200

    fix:alls bugs

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ from utils.load_file import load_file
 import os, sys  # systeme
 
 verify = input(
-    "Bienvenue sur le mini analyseur virtuel tapez q pour sortir et une autre lettre que q pour continuer: "
+    "Bienvenue sur le mini analyseur virtuel tapez q pour sortir et une autre lettre que Q pour continuer: "
 )
-if verify == "q":
-    sys.exit()
+if verify == "Q".lower():
+    sys.exit(2)
 else:
     file = input("Voulez vous analyser quel fichier à partir du dossier data ? : ")
     folder = "data"
@@ -29,18 +29,28 @@ else:
         if not os.path.exists(root):
             raise FileNotFoundError
         df = load_file(root)
-        print("Voici les metriques globales de votre fichier :\n")
+        print("Voici les metriques globales de votre fichier :")
         report = get_cleaning_report(df)
         resumé = f"Lignes : {report["rows"]}\ncollones :{report["columns"]} \nvaleures manquantes : {report["missing_values"]}\ndoublons : {report["duplicates"]}\nusage mémoire : {report["memory_mb_used"]}"
         print(resumé)
-        print(
-            f"👀 jetter un oeil à votre fichier voici les premieres lignes :\n{df.head()}"
+        verify = input("Voulez vous continuer et passer à l'étape suivante [Y,N]: ")
+        if verify == "Y".lower():
+            print(
+                f"\n👀 jetter un oeil à votre fichier voici les premieres lignes :\n{df.head()}"
+            )
+            missing = delete_duplicates(df)
+            missing = drop_missing_values(df)
+        else:
+            sys.exit(1)
+        verify = input(
+            "Maintenat voulez vous passer à votre fichier en mode nettoyage [Y,N] :"
         )
-        missing = delete_duplicates(df)
-        missing = drop_missing_values(df)
-        print(
-            f"\nAprès nettoyage des cases vides, et des doublons voici un appercu sur vos données : \n{missing.head()}"
-        )
+
+        if verify == "Y".lower():
+            print(
+                f"\nAprès nettoyage des cases vides, et des doublons voici un appercu sur vos données : \n{missing.head()}"
+            )
+            sys.exit(0)
 
     except FileNotFoundError:
         print("Fichier non existant")
