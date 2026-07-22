04550d125625ec844f12c4c5e03350033b8aa5ed
geniruphin-junior <geniruphin@gmail.com>
2026-07-22 12:13:09 +0200

    refactor:indent main.py

--- main.py
+++ main.py
@@ -19,28 +19,28 @@ import os, sys
 verify = input(
     "Bienvenue sur le mini analyseur virtuel tapez q pour sortir et c pour continuer: "
 )
-file = input("Voulez vous analyser quel fichier à partir du dossier data ? : ")
-folder = "data"
-root = os.path.join(folder, file)
-
-try:
-    if os.path.exists(root):
-        df = load_file(root)
-
-        print("Voici les metriques globales de votre fichier :\n")
-        report = get_cleaning_report(df)
-        resumé = f"Lignes : {report["rows"]}\ncollones :{report["columns"]} \nvaleures manquantes : {report["missing_values"]}\ndoublons : {report["duplicates"]}\nusage mémoire : {report["memory_mb_used"]}"
-        print(resumé)
-        print(
-            f"👀 jetter un oeil à votre fichier voici les premieres lignes :\n{df.head()}"
-        )
-        missing = delete_duplicates(df)
-        missing = drop_missing_values(df)
-        print(
-            f"\nAprès nettoyage des cases vides, et des doublons voici un appercu sur vos données : \n{missing.head()}"
-        )
-
-    else:
-        raise FileNotFoundError
-except FileNotFoundError:
-    print("Fichier non existant")
+if verify == "q":
+    sys.exit()
+else:
+    file = input("Voulez vous analyser quel fichier à partir du dossier data ? : ")
+    folder = "data"
+    root = os.path.join(folder, file)
+    try:
+        if os.path.exists(root):
+            df = load_file(root)
+            print("Voici les metriques globales de votre fichier :\n")
+            report = get_cleaning_report(df)
+            resumé = f"Lignes : {report["rows"]}\ncollones :{report["columns"]} \nvaleures manquantes : {report["missing_values"]}\ndoublons : {report["duplicates"]}\nusage mémoire : {report["memory_mb_used"]}"
+            print(resumé)
+            print(
+                f"👀 jetter un oeil à votre fichier voici les premieres lignes :\n{df.head()}"
+            )
+            missing = delete_duplicates(df)
+            missing = drop_missing_values(df)
+            print(
+                f"\nAprès nettoyage des cases vides, et des doublons voici un appercu sur vos données : \n{missing.head()}"
+            )
+        else:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        print("Fichier non existant")
