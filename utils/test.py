09fe16039a5d25731c4ba96c379ab9f7c306d56d
geniruphin-junior <geniruphin@gmail.com>
2026-08-06 14:07:46 +0200

    feat: correct test

--- utils/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("bonus.csv")
-total = df.groupby("Produit")["Prix"].mean()
-
-produit = total.sort_values(by="Prix", ascending=True)  # du plus petit au plus grand
-print(produit)
